9bb00ca16aeb22fed5edc2ecd3a04d789e28d674
Piyush Raj <81475553+piyushraj0031@users.noreply.github.com>
2024-05-02 11:33:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/run_simulation')
 def run_simulation():
     try:
-        subprocess.run(['python', 'simulation.py'], check=True)
-        return 'Simulation completed successfully!'
+        subprocess.run(['python','simulation.py'], check=True)
+        return render_template('Success.html')
     except subprocess.CalledProcessError as e:
         return f'Error running simulation: {e}', 500
 
